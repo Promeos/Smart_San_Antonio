bf100bd7b452da1b760f955f7f6c91b854971a30
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-06-20 00:18:41 -0500

    Acquired air quality and weather datasets

--- src/acquire.py
+++ src/acquire.py
@@ -1,33 +1,73 @@
 import pandas as pd
-import numpy as np
-
 import os
 
 
 def get_zone_data(sensor='ambient_noise', zone='all'):
     '''
-    Noise sensor data from a zone(s) in San Antonio.
+    Sensor data from a zone(s) in San Antonio.
     
     Parameters
     ----------
+    sensor : str optional, default 'ambient_noise'
+        The type of raw sensor data to return.
+        
+        sensor options
+        --------------
+        'air_quality'   : Air Quality sensor data
+        'ambient_noise' : Ambient Noise sensor data
+        'flood_level'   : Flood level sensor data
+        'weather'       : Weather sensor data
+        
+    
     zone : str optional, default 'all' 
         Zone where the sensors are located.
         
-        Brooks City Base = 'brooks'
-        Downtown San Antonio = 'downtown'
-        Medical Center = 'medical'
-        All zones = 'all'
+        zone options
+        ------------
+        'brooks'   : Brooks City Base
+        'downtown' : Downtown San Antonio
+        'medical'  : Medical Center
+        'all'      : All zones. Brooks, Downtown, and Medical
     
     Returns
     -------
     data : pandas.core.frame.DataFrame
-        Noise sensors dataset.
+        Raw sensor dataset.
     '''
     
     filepath = f'./data/raw/{sensor}/{zone}.csv'
-    
     try:
         if os.path.isfile(filepath):
             return pd.read_csv(filepath)
     except:
         raise FileNotFoundError(f"You're missing the {zone}.csv file.")
+    
+    
+def combine_data(sensor='ambient_noise'):
+    '''
+    For a specific sensor, combine the zones together into a file named 'all'
+    Parameters
+    ----------
+    sensor : str optional, default 'ambient_noise'
+        The type of raw sensor data to return.
+        
+        sensor options
+        --------------
+        'air_quality'   : Air Quality sensor data
+        'ambient_noise' : Ambient Noise sensor data
+        'flood_level'   : Flood level sensor data
+        'weather'       : Weather sensor data
+    '''
+    
+    a = pd.read_csv(f'./data/raw/{sensor}/brooks.csv')
+    b = pd.read_csv(f'./data/raw/{sensor}/downtown.csv')
+    c = pd.read_csv(f'./data/raw/{sensor}/medical.csv')
+    
+    merge_1 = pd.concat([a, b])
+    merge_2 = pd.concat([merge_1, c])
+    
+    merge_2.to_csv(f'./data/raw/{sensor}/all.csv', index=False)
+    
+    return merge_2
+    
+    
--- src/prepare.py
+++ src/prepare.py
@@ -2,84 +2,134 @@ import pandas as pd
 import numpy as np
 
 
-def prep_data(data, folder='ambient_noise', filename='all'):
+def prep_data(data, filename='air'):
     '''
-    Prepare the noise sensor data.
+    Prepare the sensor data. If a prepared version of the sensor does not exist,
+    one is created for the user.
     
     Parameters
     ----------
     data : pandas.core.frame.DataFrame
-        The raw noise sensor data from the raw/ data file.
-    
+        Raw sensor data.
+        
+    filename : str, default 'air'
+        The type of prepared sensor data
+        
+        filename options
+        ----------------
+        'air' : Returns air quality data for all zones. 
+        'noise' : Returns ambient noise data for all zones.
+        'water' : Returns water level data for all zones.
+        'weather' : Returns weather data for all zones.
+
     Returns
     -------
     data : pandas.core.frame.DataFrame
-        Prepared noise sensor data.
+        Prepared ensor data.
     '''
-    filepath = f'./data/prepared/{folder}/{filename}.csv'
+    filepath = f'./data/prepared/{filename}.csv'
     
     try:
         data = pd.read_csv(filepath)
     except FileNotFoundError:
-        data.columns = [col.lower() for col in data.columns]
-
-        data.rename(columns={'datetime': 'date',
-                            'sensormodel': 'sensor_model',
-                            'lat': 'latitude',
-                            'long': 'longitude',
-                            'noiselevel_db': 'noise_level',
-                            'alerttriggered': 'alert_triggered',
-                            'sensorstatus': 'sensor_status'},
-                    inplace=True)
-
-        data['date'] = pd.to_datetime(data['date'], infer_datetime_format=True)
-        data.alert_triggered = np.where(data.alert_triggered == np.NaN, 'Not Supported', 'No')
-        data.zone = data.zone.replace(to_replace=" Market\s\d{2}", value='', regex=True)
-
-        # Replace duplicate latitude and longitude data with the most recent coordinates.
-        data.loc[data.sensor_id == '64d7e0e4cfeba0b6', ('latitude', 'longitude')] = data.loc[(data.sensor_id == '64d7e0e4cfeba0b6')&(data.date == data.date.max()), ('latitude', 'longitude')].max().values
-
-        data.loc[data.sensor_id == '64d7e4dbde37ccb5', ('latitude', 'longitude')] = data.loc[(data.sensor_id == '64d7e4dbde37ccb5')&(data.date == data.date.max()), ('latitude', 'longitude')].max().values
+        data = normalize_common_cols(data)
         
+        if filename == 'air':
+            data = prep_air_data(data)
+        elif filename == 'noise':
+            data = prep_noise_data(data)
+        elif filename == 'water':
+            data = prep_water_data(data)
+        elif data == 'weather':
+            data = prep_weather_data(data)
+        else:
+            return None
         data.to_csv(filepath, index=False)
     finally:
         return data
 
-   
-# def prep_noise_data(data):
-#     '''
-    
-#     '''
-#     data.columns = [col.lower() for col in data.columns]
-#     data = rename_common_cols(data)
-#     data.rename(columns={'noiselevel_db': 'noise_level'},
-#                 inplace=True)
-#     return data
-  
-   
-    
-# def prep_water_data():
-#     '''
-    
-#     '''
-    
-    
-# def rename_common_cols(data):
-#     '''
-#     Rename columns shared across all sensor types.
-#     '''
-#     data.columns = [col.lower() for col in data.columns]
-#     data.rename(columns={'datetime': 'date',
-#                          'sensormodel': 'sensor_model',
-#                          'lat': 'latitude',
-#                          'long': 'longitude',
-#                          'alerttriggered': 'alert_triggered',
-#                          'sensorstatus': 'sensor_status'},
-#                 inplace=True)
-#     return data
 
+#################################################################################################################################
+
+
+def prep_air_data(data):
+    '''
+    Prepare the air quality sensor data for exploration.
+    '''
+
+    return data
+
+
+def prep_noise_data(data):
+    '''
+    Prepare the ambient noise data for exploration.
+    '''
+    data.rename(columns={'noiselevel_db': 'noise_level'},
+                inplace=True)
+    data.zone = data.zone.replace(to_replace=" Market\s\d{2}", value='', regex=True)
+    data = update_sensor_coordinates(data)
+    return data
+  
     
-#     data['date'] = pd.to_datetime(data['date'], infer_datetime_format=True)
-#     data.alert_triggered = np.where(data.alert_triggered == np.NaN, 'Not Supported', 'No')
-#     data.zone = data.zone.replace(to_replace=" Market\s\d{2}", value='', regex=True)
+def prep_water_data(data):
+    '''
+    Prepare the water level sensor data for exploration.
+    '''
+    data.drop(columns=['disttowl_m', 'disttodf_m', 'temp_c'],
+              inplace=True)
     
+    data.rename(columns={'disttowl_ft': 'dist_to_water_ft',
+                         'disttodf_ft': 'dist_to_floor_ft'},
+                inplace=True)
+    
+    data['water_level'] = data['dist_to_floor_ft'] - data['dist_to_water_ft']
+    return data
+
+
+def prep_weather_data(data):
+    '''
+    Prepare the weather data for exploration.
+    '''
+    
+    
+    return data
+    
+    
+    
+
+#################################################################################################################################
+
+
+def update_sensor_coordinates(data):
+    '''
+    Update all previous coordinates with the most recent position of the sensor.
+    
+    ONLY APPLICABLE TO THE NOISE SENSOR DATASET
+    
+    Refactor to update future lat and long artifacts.
+    '''
+    data.loc[data.sensor_id == '64d7e0e4cfeba0b6', ('latitude', 'longitude')] = \
+        data.loc[(data.sensor_id == '64d7e0e4cfeba0b6')&(data.date == data.date.max()), ('latitude', 'longitude')].max().values
+    data.loc[data.sensor_id == '64d7e4dbde37ccb5', ('latitude', 'longitude')] = \
+        data.loc[(data.sensor_id == '64d7e4dbde37ccb5')&(data.date == data.date.max()), ('latitude', 'longitude')].max().values
+    
+    return data
+
+
+def normalize_common_cols(data):
+    '''
+    Rename columns shared across all sensor types.
+    '''
+    data.columns = [col.lower() for col in data.columns]
+    data.rename(columns={'datetime': 'date',
+                         'sensormodel': 'sensor_model',
+                         'lat': 'latitude',
+                         'long': 'longitude',
+                         'alerttriggered': 'alert_triggered',
+                         'sensorstatus': 'sensor_status'},
+                inplace=True)
+    
+    data['date'] = pd.to_datetime(data['date'], infer_datetime_format=True)
+    data.alert_triggered = np.where(data.alert_triggered == np.NaN, 'Not Supported', 'No')
+    
+    return data
